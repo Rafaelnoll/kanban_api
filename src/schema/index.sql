caa23a5989d16098384a0111a9e8d0bf302a2d96
Rafael Noll <rafanoll2004@gmail.com>
2023-06-14 12:06:45 -0300

    Chore(database): Adding status field on sql schema

--- src/schema/index.sql
+++ src/schema/index.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE kanban;
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';
 
 CREATE TABLE IF NOT EXISTS categories(
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_V4(),
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS categories(
 CREATE TABLE IF NOT EXISTS tasks(
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_V4(),
   title VARCHAR NOT NULL,
+  status VARCHAR NOT NULL CHECK (status IN ('DO', 'DOING', 'DONE')),
   description VARCHAR,
   category_id UUID,
   FOREIGN KEY (category_id) REFERENCES categories(id)
