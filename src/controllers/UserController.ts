d7a4685436a6e179745b49f08eae51b6dffa6539
Rafael Noll <rafanoll2004@gmail.com>
2023-07-10 13:04:34 -0300

    Feature: Adding route to create a user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,39 @@
 import { Request, Response } from 'express';
 
+import HashGenetator from '../utils/HashGenetator';
 import UserRepository from '../repositories/UserRepository';
+import IUser from '../interfaces/User';
 
 class UserController {
   async index(request: Request, response: Response) {
     const users = await UserRepository.findAll();
     response.status(200).json(users);
+  }
+
+  async store(request: Request, response: Response) {
+    const { username, email, password }: IUser = request.body;
+
+    if (!username) {
+      return response.status(400).json({ error: 'Username is required' });
+    }
+
+    if (!email) {
+      return response.status(400).json({ error: 'Email is required' });
+    }
+
+    if (!password) {
+      return response.status(400).json({ error: 'Password is required' });
+    }
+
+    const hashOfPassword = HashGenetator.passwordToHash(password);
+
+    await UserRepository.create({
+      username,
+      email,
+      password: hashOfPassword,
+    });
+
+    response.sendStatus(204);
   }
 }
 
--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,8 +1,22 @@
+import IRepository from '../interfaces/Repository';
+import IUser from '../interfaces/User';
 import query from '../database';
 
-class UserRepository {
+class UserRepository implements Partial<IRepository<IUser>> {
   async findAll() {
     return await query('SELECT * FROM users;');
+  }
+
+  async create({ username, email, password }: IUser) {
+    const [row] = await query(
+      `
+      INSERT INTO users(username, email, password)
+      VALUES($1,$2,$3)
+      RETURNING *
+    `,
+      [username, email, password],
+    );
+    return row;
   }
 }
 
