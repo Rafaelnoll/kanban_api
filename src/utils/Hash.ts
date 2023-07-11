006ac73f6725325e7b0972def94e24a53235cb7a
Rafael Noll <rafanoll2004@gmail.com>
2023-07-11 10:30:51 -0300

    Refactor: Removing secret from Hash.ts

--- src/utils/Hash.ts
+++ src/utils/Hash.ts
@@ -2,13 +2,9 @@ import bcrypt from 'bcrypt';
 
 class Hash {
   private saltRounds: number;
-  private secret: string;
 
   constructor() {
     this.saltRounds = 10;
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    this.secret = process.env.HASH_SECRET!;
-
     this.passwordToHash = this.passwordToHash.bind(this);
   }
 
