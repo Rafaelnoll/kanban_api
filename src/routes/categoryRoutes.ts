db4403a86eb7b53de2a5efd479a99b196345e93d
Rafael Noll <rafanoll2004@gmail.com>
2023-07-12 08:40:08 -0300

    Chore: Organizing the routes by HTTP methods

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -4,10 +4,17 @@ import CategoryController from '../controllers/CategoryController';
 
 const router = Router();
 
+// GET
 router.get('/categories/:id', CategoryController.show);
 router.get('/categories', CategoryController.index);
+
+// POST
 router.post('/categories', CategoryController.store);
+
+// PUT
 router.put('/categories/:id', CategoryController.update);
+
+// Delete
 router.delete('/categories/:id', CategoryController.delete);
 
 export default router;
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -3,11 +3,18 @@ import TaskController from '../controllers/TaskController';
 
 const router = express.Router();
 
+// GET
 router.get('/tasks/category/:category_id', TaskController.listAllByCategory);
 router.get('/tasks/:id', TaskController.show);
 router.get('/tasks', TaskController.index);
+
+// POST
 router.post('/tasks', TaskController.store);
+
+// PUT
 router.put('/tasks/:id', TaskController.update);
+
+// Delete
 router.delete('/tasks/:id', TaskController.delete);
 
 export default router;
