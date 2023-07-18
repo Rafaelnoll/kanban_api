8bbe53d00ac3f929e0533875a90a769db0e644f4
Rafael Noll <rafanoll2004@gmail.com>
2023-07-18 14:00:06 -0300

    Chore(schema): Adding FK user_id in categories table

--- src/schema/index.sql
+++ src/schema/index.sql
@@ -2,10 +2,21 @@ CREATE DATABASE kanban;
 
 CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';
 
+CREATE TABLE IF NOT EXISTS users(
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_V4(),
+  username VARCHAR NOT NULL UNIQUE,
+  email VARCHAR NOT NULL UNIQUE,
+  description VARCHAR,
+  password VARCHAR NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS categories(
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_V4(),
   name VARCHAR NOT NULL,
+  user_id UUID NOT NULL
   PRIMARY KEY (id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS tasks(
@@ -17,13 +28,4 @@ CREATE TABLE IF NOT EXISTS tasks(
   user_id UUID NOT NULL,
   FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
   FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
-CREATE TABLE IF NOT EXISTS users(
-  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_V4(),
-  username VARCHAR NOT NULL UNIQUE,
-  email VARCHAR NOT NULL UNIQUE,
-  description VARCHAR,
-  password VARCHAR NOT NULL,
-  PRIMARY KEY (id)
 );
